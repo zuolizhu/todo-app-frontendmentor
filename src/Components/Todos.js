c8b9b75f42c250daf948306f00a17d408083df7c
Connor <caffeineoverdosed@Caffeines-MacBook-Air.local>
2020-12-01 14:57:44 -0500

    drag n drop function added

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -3,6 +3,28 @@ import Loader from './Loader'
 import useStore from '../store'
 import { useEffect } from 'react'
 import useFetch from '../Hooks/useFetch'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
+const grid = 8;
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 250
+});
+
 
 export default function Todos() {
   const { get, loading } = useFetch("http://my-json-server.typicode.com/zuolizhu/json-server-data/")
@@ -17,12 +39,61 @@ export default function Todos() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list)
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed)
+    return result
+  }
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) {
+      return
+    }
+
+    const updatedTodos = reorder(
+      todos,
+      result.source.index,
+      result.destination.index
+    )
+
+    setTodos(updatedTodos)
+  }
+
+  if (loading) return <Loader />
+
   return (
     <>
-      {loading && <Loader />}
-      {todos.map(todo => (
-        <Todo key={todo.id} todo={todo} />
-      ))}
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {todos.map((todo, index) => (
+                <Draggable key={todo.id} draggableId={`todo-${todo.id}`} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      <Todo key={todo.id} todo={todo} />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </>
   )
 }
